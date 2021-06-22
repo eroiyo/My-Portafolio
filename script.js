bc25abefe0b7d89cd51f5ac5cb648e4165356b90
SimonGrchevski <simongrcevski@gmail.com>
2021-06-22 13:19:11 +0200

    Add the logic for the logo hidden

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 
 const nav2 = document.querySelector('.secondary-nav')
 const screenSize = window.matchMedia('(min-width: 1024px)');
+const logo = document.querySelector('.logo');
 
 function clickFunction() {
     var para = document.getElementById('toggle-icon');
     para.classList.toggle('rotate-icon');
-    if(nav2.style.display!='flex' ){
-        nav2.style.display='flex'
-    }else{
-        nav2.style.display='none'
+    if (nav2.style.display != 'flex') {
+        nav2.style.display = 'flex'
+        logo.style.display = 'hidden';
+
+    } else {
+        nav2.style.display = 'none';
+        logo.style.display = 'inline-block';
     }
 }
 
 function phone(screenSize) {
     if (screenSize.matches) {
-        if(nav2.style.display=='flex'){
+        if (nav2.style.display == 'flex') {
             clickFunction();
         }
     }
-    else{
+    else {
     }
 }
 phone(screenSize);

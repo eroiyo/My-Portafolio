89e2eb7e221ad47693172b76902506952637ead9
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-22 08:16:53 -0400

    Fix Linter error III

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const screenSize = window.matchMedia('(min-width: 1024px)');
 const logo = document.querySelector('.logo');
 
 function clickFunction() {
-    let para = document.getElementById('toggle-icon');
+    const para = document.getElementById('toggle-icon');
     para.classList.toggle('rotate-icon');
     if (nav2.style.display != 'flex') {
-        nav2.style.display = 'flex'
-        logo.classList.remove('not-hidden')
-        logo.classList.add('hidden');
+    nav2.style.display = 'flex'
+    logo.classList.remove('not-hidden')
+    logo.classList.add('hidden');
     } else {
-        nav2.style.display = 'none';
-        logo.classList.add('not-hidden')
-        logo.classList.remove('hidden');
-    }
+    nav2.style.display = 'none';
+    logo.classList.add('not-hidden')
+    logo.classList.remove('hidden');
+  }
 }
 
 function phone(screenSize) {
-    if (screenSize.matches) {
-        if (nav2.style.display === 'flex') {
-            clickFunction();
-        }
+  if (screenSize.matches) {
+    if (nav2.style.display === 'flex') {
+      clickFunction();
     }
+  }
 }
 phone(screenSize);
 screenSize.addListener(phone);

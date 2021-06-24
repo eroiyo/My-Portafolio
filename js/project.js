2b636826515acfc80c9abed5ac3e221aeb0d0dc2
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-24 05:14:06 -0400

    Put message visibility hidden (Fix Linter error II)

--- js/project.js
+++ js/project.js
@@ -1,34 +1,34 @@
 import data from './data.js';
-function Project(id, title, overview, description, big_image, image, alt, big_alt, tags, live, repo) {
+function Project(id, title, view, description, big_Image, image, alt, big_Alt, tags, live, repo) {
   if (image !== undefined) {
-    this.image = image
+    this.image = image;
   } else {
     this.image = 'assest/placeholder.png';
   }
-  if (big_image !== undefined) {
-    this.big_image = big_image
+  if (big_Image !== undefined) {
+    this.big_Image = big_Image;
   } else {
-    this.big_image = 'assest/Big.png';
+    this.big_Image = 'assest/Big.png';
   }
   if (title !== undefined) {
     this.title = title;
   } else {
-    this.title = 'Multi-Post Stories'
+    this.title = 'Multi-Post Stories';
   }
   if (alt !== undefined) {
     this.alt = alt;
   } else {
-    this.alt = 'placeholder'
+    this.alt = 'placeholder';
   }
-  if (big_alt !== undefined) {
-    this.big_alt = big_alt;
+  if (big_Alt !== undefined) {
+    this.big_Alt = big_Alt;
   } else {
-    this.big_alt = 'big_placeholder'
+    this.big_Alt = 'big_placeholder';
   }
-  if (overview !== undefined) {
-    this.overview = overview;
+  if (view !== undefined) {
+    this.overview = view;
   } else {
-    this.overview = "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text."
+    this.overview = "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.";
   }
   if (description !== undefined) {
     this.description = description;
@@ -48,12 +48,12 @@ function Project(id, title, overview, description, big_image, image, alt, big_al
   if (repo !== undefined) {
     this.repo = repo;
   } else {
-    this.repo = '#'
+    this.repo = '#';
   }
   if (id !== undefined) {
     this.id = id;
   } else {
-    this.id = '?'
+    this.id = '?';
   }
 
   this.appear = function (right) {
@@ -63,39 +63,39 @@ function Project(id, title, overview, description, big_image, image, alt, big_al
     const first = document.createElement('div');
     first.classList.add('first');
     list.appendChild(box);
-    const part_a = document.createElement('div');
-    part_a.classList.add('part-a')
+    const part_A = document.createElement('div');
+    part_A.classList.add('part-a')
     const sm_img = document.createElement('img');
     sm_img.src = this.image;
     sm_img.alt = this.alt;
     sm_img.classList.add('placeholder');
-    part_a.appendChild(sm_img);
+    part_A.appendChild(sm_img);
     const latest = document.createElement('div');
     latest.classList.add('latest');
-    const part_b = document.createElement('div');
-    part_b.classList.add('part_b');
-    const project_title = document.createElement('h2');
-    project_title.classList.add('project-title');
-    project_title.innerHTML = this.title;
-    part_b.appendChild(project_title);
-    const project_info = document.createElement('p');
-    project_info.classList.add('project-info');
-    project_info.innerHTML = this.overview;
-    part_b.appendChild(project_info);
+    const part_B = document.createElement('div');
+    part_B.classList.add('part-b');
+    const project_Title = document.createElement('h2');
+    project_Title.classList.add('project-title');
+    project_Title.innerHTML = this.title;
+    part_B.appendChild(project_Title);
+    const project_Info = document.createElement('p');
+    project_Info.classList.add('project-info');
+    project_Info.innerHTML = this.overview;
+    part_B.appendChild(project_Info);
     const tags = document.createElement('div');
     tags.classList.add('tags');
-    for (let i in this.tags) {
-      let span = document.createElement('span');
+    for (let i=0; i<tags.length; i++) {
+      const span = document.createElement('span');
       span.innerHTML = this.tags[i];
-      var rectangle = document.createElement('img');
+      const rectangle = document.createElement('img');
       rectangle.classList.add('rectangle');
-      rectangle.src = 'assest/Rectangle.png'
-      rectangle.alt = 'Rectangle'
+      rectangle.src = 'assest/Rectangle.png';
+      rectangle.alt = 'Rectangle';
       tags.appendChild(span);
       tags.appendChild(rectangle);
     }
     rectangle.remove();
-    part_b.appendChild(tags)
+    part_B.appendChild(tags)
     const button = document.createElement('button');
     button.classList.add('button');
     button.type = 'button';
@@ -105,17 +105,17 @@ function Project(id, title, overview, description, big_image, image, alt, big_al
     const button_span = document.createElement('span');
     button_span.innerHTML = 'See Project';
     button.appendChild(button_span);
-    part_b.appendChild(button);
+    part_B.appendChild(button);
 
     if (right == 0) {
-      first.appendChild(part_a);
-      latest.appendChild(part_b);
+      first.appendChild(part_A);
+      latest.appendChild(part_B);
       box.appendChild(first);
       box.appendChild(latest)
       box.classList.add('project-card');
     } else {
-      first.appendChild(part_b);
-      latest.appendChild(part_a);
+      first.appendChild(part_B);
+      latest.appendChild(part_A);
       box.appendChild(latest);
       box.appendChild(first)
       box.classList.add('project-card2');
@@ -128,7 +128,7 @@ function Project(id, title, overview, description, big_image, image, alt, big_al
     const modal_title = document.querySelector('.modal-title')
     modal_title.textContent = this.title;
     const modal_image = document.querySelector('.modal-img');
-    modal_image.src = this.big_image;
+    modal_image.src = this.big_Image;
     const modal_info = document.querySelector('.modal-info');
     modal_info.textContent = this.description;
     const modal_tags = document.querySelector('#modal-tags')
@@ -160,7 +160,7 @@ function Project(id, title, overview, description, big_image, image, alt, big_al
   const bt = document.querySelector('.modal-buttons')
   function callp(num) {
     let buffer = myProjects[num];
-    let p = new Project(buffer.id, buffer.title, buffer.overview, buffer.description, buffer.big_image, buffer.image, buffer.alt, buffer.alt, undefined, buffer.live, buffer.repo);
+    let p = Project(b.id, b.title, b.view, b.desc, b.big_img, b.img, b.alt, b.big_Alt, b.tag, b.live, b.repo);
     p.message();
     bt.style.display = "flex"
     modal.style.visibility = "visible";
